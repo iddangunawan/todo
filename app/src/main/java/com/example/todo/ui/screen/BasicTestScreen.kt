07cf2bfef5919582f3ea336e6f8ceca4d0fe571c
Iddan Gunawan <iddandunk@gmail.com>
2024-01-15 02:41:30 +0700

    Basic Test 2

--- app/src/main/java/com/example/todo/ui/screen/BasicTestScreen.kt
+++ app/src/main/java/com/example/todo/ui/screen/BasicTestScreen.kt
@@ -30,14 +30,18 @@ fun BasicTestScreen(
         Text(
             text = "Answer: ${miniMaxSum(listOf(1, 3, 5, 7, 9))}",
             style = MaterialTheme.typography.titleMedium,
-            maxLines = 1,
             overflow = TextOverflow.Ellipsis,
         )
         Spacer(modifier = Modifier.height(16.dp))
         Text(
-            text = "Test 2",
+            text = "Test 2 | listOf(1, 1, 0, -1, -1)",
             style = MaterialTheme.typography.titleMedium,
             maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+        )
+        Text(
+            text = "Answer: ${plusMinus(listOf(1, 1, 0, -1, -1))}",
+            style = MaterialTheme.typography.titleMedium,
             overflow = TextOverflow.Ellipsis,
         )
         Spacer(modifier = Modifier.height(16.dp))
@@ -61,4 +65,33 @@ fun miniMaxSum(arr: List<Int>): String {
     val maxSum = sortedArr.takeLast(4).sum()
 
     return "$minSum $maxSum"
+}
+
+fun plusMinus(arr: List<Int>): String {
+    // Initialization of counts for positive, negative, and zero values
+    var positiveCount = 0
+    var negativeCount = 0
+    var zeroCount = 0
+
+    // Check whether the array elements are positive, negative, or zero
+    for (num in arr) {
+        when {
+            num > 0 -> positiveCount++
+            num < 0 -> negativeCount++
+            else -> zeroCount++
+        }
+    }
+
+    // Calculate the total number of elements in the array
+    val total = arr.size.toDouble()
+
+    // Calculate the ratio of positive, negative, and zero values to the total number of elements
+    val positiveRatio = positiveCount / total
+    val negativeRatio = negativeCount / total
+    val zeroRatio = zeroCount / total
+
+    // Print the ratios with a format of six decimal places
+    return "\n %.6f".format(positiveRatio) +
+            "\n %.6f".format(negativeRatio) +
+            "\n %.6f".format(zeroRatio)
 }
